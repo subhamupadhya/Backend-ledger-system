49c92fb1cb6ffdb097ea9eff474a20add1896a56
Subham Kumar <subhamupadhya53100@gmail.com>
2026-03-28 16:59:41 +0530

    user model completed

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ connectToDB()
 app.listen(3000, ()=> {
     console.log("server is running on port 3000")
 })
+
+
+
+
+
+
+
+
+
+
+
+
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
-const userSchema = mongoose.Schema({
+
+const userSchema = new mongoose.Schema({
     email:{
         type:String,
         required:[true,"Email is required for creating a user"],
@@ -17,6 +19,105 @@ const userSchema = mongoose.Schema({
     }, 
     password:{
         type: String,
-        REQUIRED:[]
+        required:[true,"Password is required for creating an account"],
+        minlength:[6,"password should be contain more than 6 character"],
+        select: false
     }
+}, {
+    timestamps: true
+});
+
+
+userSchema.pre("save", async function(next){
+ 
+    if(!this.isModified("password")) {
+        return next()
+    }
+
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+
+    return next()
 })
+
+userSchema.methods.comparePassword = async function(password){
+
+    return await bcrypt.compare(password, this.password)
+}
+
+
+const userModel = mongoose.model("user", userSchema)
+
+
+module.exports = userModel
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
